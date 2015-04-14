d3f4e5296a722b56b20294f8f6c5b8dc8c102b1e
Andrew Krug <andrew.m.krug@gmail.com>
2015-04-14 12:16:32 -0400

    forgot to add the phantom js file for capability

---
+++ SeleniumGridExtras/src/main/java/com/groupon/seleniumgridextras/config/capabilities/PhantomJS.java
@@ -0,0 +1,14 @@
+package com.groupon.seleniumgridextras.config.capabilities;
+
+public class PhantomJS extends Capability {
+
+  @Override
+  public String getWebDriverClass() {
+    return "org.openqa.selenium.phantomjs";
+  }
+
+  @Override
+  public String getIcon() {
+    return "AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAD///8A////AP///wD///8A////APf1zDLr5oJ88e6qVPv65hj9/fQK8/C2SOfhZpnq5X6B/v34Bv///wD///8A////APDsoF708rxC+/vqFPn43CLm4GSb491SreLbSLbl3lql5+FqlOTdVKvl3lql4ttMs+zoinT29Moz+PfWKP///wDe1jLN29Ia5eHaRLvk3lin5t9gn+XfXqHj3VKt5N5Yp+bfYJ/m4GKc5N1WqODYOsXh2UC/49xQruPcTrH///8A6+aCfNrQFOrb0hzj3dUq1eDZPsHf1zbJ3dQo19/XNMri20i25N1WqN7WMs3a0Rjn3NMg3tzUJNvd1SzT////APb0yjPb0hzj3NMi3dvSHuDb0h7g3NMg3tvSHOPb0h7g3NMg3tzTIt3c0yLd29Ie4NzTIt3a0Rjn4Ng8wv///wD9/PIM39c2ydvSHuDc0yDe3NMi3dzTIt3c0yDe3NMi3dzTIt3c0yLd3NMi3dzTIN7c0yLd2dAS7evmgnz///8A////AOTeWKfa0Rjn3NMi3dzTIt3c0yLd3NMi3dzTIt3c0yLd3NMi3dzTIN7c0yLd3NMi3drQFOrz8LZI////AP///wDo4myT2tEY59zTIt3c0yLd3NQk29zUJNvc1CTb3NQk29zUJNvc0yLd3NMi3dzTIN7b0h7g+PbSK////wD///8A6ONyjdrRFunc0yLd3NMi3drRGOfc0yDe3NMi3drRGOfc0yDe3NQk29zTIt3c0yDe3NQk2/n43h////8A////AOrlfIPa0BTq3NQk29nPDvDXzQD/180E+tjOCPfXzQD/2M4I99zTIt3c1CTb29Ie4N3VLNP7++oU////AP///wDw7aRa2tEW6dzTIt3XzQL9180A/9fNAP/XzQD/180A/9fNAP/b0h7g3NQk29vSGuXh2UK9/v76A////wD///8A+vniHNzTIN7b0h7g2c8O8NfNAP/XzQT6184G+NfNAP/Yzgj33NMi3dzUJNvZ0BLt6+aAfv///wD///8A////AP///wDo4myT180C/dvSHuDa0Rjn29Ie4NzTIN7a0Rjn3NMg3tzUJNvb0h7g3NMi3fn43CL///8A////AP///wD///8A+/rmGN7WLtDXzQL92tEY59zTIN7c1CTb3NQk29zUJNvc0yDe2dAS7e7qlmf///8A////AP///wD///8A////AP///wD499Yo4Ng6xdfNAP/Yzwzz3NMg3tvSHuDa0Rbp29Ic4+3ojnD///8A////AP///wD///8A////AP///wD///8A////AP388gzt6I5w4tpGud7WMM/f2DjH5+Fmmfbzxjj///8A////AP///wD///8A/+cAAPgHAACAAAAAwAAAAMAAAADAAQAAwAEAAMABAADAAQAAwAEAAOABAADgAwAA4AMAAPAHAAD4DwAA/h8AAA==";
+  }
+}
